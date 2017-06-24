485d221300701e849557f30d2936ab666d1f8561
Nathan Vass <nathanevass@gmail.com>
2017-06-24 13:23:53 -0400

    add dropdown menu to new story

--- frontend/components/new_story_header.jsx
+++ frontend/components/new_story_header.jsx
@@ -1,20 +1,54 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const NewStoryDropdown = () => {
+  return (
+    <ul className="new-story-dropdown">
+      <li className="new-story-dropdown-title">Actions</li>
+      <li className="new-story-dropdown-item">Delete story</li>
+    </ul>
+  );
+};
+
 class NewStoryHeader extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      showDropdown: false
+    };
+    this.removeDropdown = this.removeDropdown.bind(this);
+    this.addDropdown = this.addDropdown.bind(this);
+    this.toggleDropdown = this.toggleDropdown.bind(this);
+  }
+
+  toggleDropdown(e){
+    this.setState({
+      showDropdown: this.state.showDropdown ? false : true
+    });
+  }
+
+  removeDropdown(e){
+    this.setState({ showDropdown: false });
+  }
+
+  addDropdown(e){
+    this.setState({ showDropdown: true });
   }
 
   render(){
     return (
       <section className="new-story-header">
+        <div onClick={this.removeDropdown} className="click-listener"></div>
         <div className="new-story-share"><h5>Share</h5></div>
         <div className="new-story-publish">
           <h5>Publish</h5>
           <i className="fa fa-angle-down header-fa" aria-hidden="true"></i>
         </div>
-        <i className="fa fa-ellipsis-h header-fa" aria-hidden="true"></i>
+        <i onClick={this.toggleDropdown} className="fa fa-ellipsis-h header-fa" aria-hidden="true"></i>
+
+        {this.state.showDropdown ?
+          <NewStoryDropdown /> : null
+        }
       </section>
     );
   }
